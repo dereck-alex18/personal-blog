284a6314e522537c535cc153684bc767d5bdad0d
Dereck Portela <dereck_alexander@outlook.com>
2024-08-29 17:05:53 -0300

    add single post page

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
-    path("posts/", views.blog_posts)
+    path("", views.index, name = "index-page"),
+    path("posts/", views.blog_posts, name = "all-posts"),
+    path("post/<slug:slug>", views.blog_single_post, name="single-post")
 ]
